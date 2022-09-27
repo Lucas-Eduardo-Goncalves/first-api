3b9840f65e38b98e7737756d2edd7fd27cd70e89
Lucas Gonçalves <lucasGoncalvesGithub@gmail.com>
2022-09-27 11:51:20 -0300

    Privatizado rotas de categorias

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import multer from "multer";
 
+import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 import { CreateCategoryController } from "../modules/cars/useCases/createCategory/CreateCategoryController";
 import { ImportCategoryController } from "../modules/cars/useCases/importCategory/ImportCategoryController";
 import { ListCategoriesController } from "../modules/cars/useCases/listCategories/ListCategoriesController";
@@ -11,6 +12,9 @@ const upload = multer({
   dest: "./tmp",
 });
 
+// middlewate para validação de token
+categoriesRoutes.use(ensureAuthenticated);
+
 const listCategoriesController = new ListCategoriesController();
 categoriesRoutes.get("/", listCategoriesController.handle);
 
@@ -18,6 +22,10 @@ const createCategoryController = new CreateCategoryController();
 categoriesRoutes.post("/", createCategoryController.handle);
 
 const importCategoryController = new ImportCategoryController();
-categoriesRoutes.post("/csv", upload.single("file"), importCategoryController.handle);
+categoriesRoutes.post(
+  "/csv",
+  upload.single("file"),
+  importCategoryController.handle
+);
 
 export { categoriesRoutes };
