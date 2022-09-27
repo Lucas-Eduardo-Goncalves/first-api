bf35b873062dc513d30f2a81a5e18e5c1207050b
Lucas Gonçalves <lucasGoncalvesGithub@gmail.com>
2022-09-26 21:29:16 -0300

    Adicionado rota para gerar token

---
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
@@ -0,0 +1,56 @@
+import { compare } from "bcrypt";
+import { sign } from "jsonwebtoken";
+import { inject, injectable } from "tsyringe";
+
+import type { User } from "../../entities/User";
+import type { IUserRepository } from "../../repositories/IUserRepositories";
+
+interface IRequest {
+  email: string;
+  password: string;
+}
+
+interface IResponse {
+  token: string;
+  user: {
+    id: string;
+    name: string;
+    email: string;
+  };
+}
+
+@injectable()
+class AuthenticateUserUseCase {
+  constructor(
+    @inject("UsersRepository")
+    private userRepository: IUserRepository
+  ) {}
+
+  async execute(props: IRequest): Promise<IResponse> {
+    const user = await this.userRepository.findByEmail(props.email);
+    if (!user) {
+      throw new Error("Email or password incorrect!");
+    }
+
+    const passwordMatch = await compare(props.password, user.password);
+    if (!passwordMatch) {
+      throw new Error("Email or password incorrect!");
+    }
+
+    const token = sign({}, "fe9cee841ee513c647796fa0019e498a", {
+      subject: user.id,
+      expiresIn: "1d",
+    });
+
+    return {
+      user: {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+      },
+      token,
+    };
+  }
+}
+
+export { AuthenticateUserUseCase };
